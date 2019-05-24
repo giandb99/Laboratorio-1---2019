97a01d793b725ca6e021b665f8589cc5622c941d
Gianfranco <40763595+giandb99@users.noreply.github.com>
2019-05-24 12:34:10 -0300

    Add files via upload

---
+++ Punteros 3/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "empleado.h"
+#define T 5
+
+int main()
+{
+    eEmpleado* unEmpleado;
+    unEmpleado = nuevoEmpleadoParametros(1001,"Pedro",10000);
+
+    if(getLegajo(unEmpleado) == 1001)
+    {
+        mostrarEmpleado(unEmpleado);
+    }
+
+    return 0;
+}
+
+/*
+    int* x;
+    int i;
+    int aux;
+
+    x = (int*) malloc(sizeof(int)*T);
+
+    if(x!=NULL)
+    {
+        for(i=0;i<T;i++)
+        {
+          printf("%d\n", *x);
+        }
+    }
+
+
+    x = (int*) calloc(sizeof(int),T);
+
+    if(x!=NULL)
+    {
+        for(i=0; i<T; i++)
+        {
+          printf("Ingrese un numero: ");
+          scanf("%d", x+i);
+        }
+
+        for(i=0; i<T; i++)
+        {
+          printf("\n%d", *(x+i));
+        }
+
+        printf("\n\nAgrego 5 mas: \n");
+
+        aux = (int*)realloc(x,sizeof(int)*(T+5));
+
+        if(aux!=NULL)
+        {
+            x=aux;
+            for(i=T; i<T+5; i++)
+            {
+                printf("Ingrese un numero: ");
+                scanf("%d", (x+i));
+            }
+
+            for(i=0; i<T+5; i++)
+            {
+                printf("\n%d", *(x+i));
+            }
+
+            printf("\n\nQuito 4:\n");
+            x=(int*)realloc(x,sizeof(int)*(T+5-4));
+
+            for(i=0; i<T+5-4; i++)
+            {
+                printf("\n%d", *(x+i));
+            }
+        }
+    }*/
+
